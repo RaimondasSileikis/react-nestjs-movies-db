2d7392ef9f6446f9dc5a655ba4a9e36353ec8610
Raimondas <raimondas.sileikis@gmai.com>
2023-07-27 17:59:29 +0300

    user module

--- movies-db-server/src/user/user.service.ts
+++ movies-db-server/src/user/user.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { EditUserDto } from './dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+  async editUser(userId: number, dto: EditUserDto) {
+    const user = await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        ...dto,
+      },
+    });
+    delete user.password;
+    return user;
+  }
+}
