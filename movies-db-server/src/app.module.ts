4cce7f115566450e46929654f41164d65d3f5a95
Raimondas <raimondas.sileikis@gmai.com>
2023-07-27 16:23:57 +0300

    prisma service, module

--- movies-db-server/src/app.module.ts
+++ movies-db-server/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 import { MovieModule } from './movie/movie.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, AuthModule, MovieModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    UserModule,
+    MovieModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
