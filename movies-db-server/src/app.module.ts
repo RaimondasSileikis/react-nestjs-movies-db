dc4ed07a9d0cb80f34fd9342af8c96380f94b4c9
Raimondas <raimondas.sileikis@gmai.com>
2023-07-27 15:23:45 +0300

    initial commit auth, user, movies

--- movies-db-server/src/app.module.ts
+++ movies-db-server/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, AuthModule, MoviesModule],
 })
 export class AppModule {}
