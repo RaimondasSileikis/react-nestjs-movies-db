74323c8578e88f27bc56de8088a4f67b86f02283
Raimondas <raimondas.sileikis@gmai.com>
2023-07-28 15:44:48 +0300

    Header

--- movies-db/src/components/Header.tsx
+++ movies-db/src/components/Header.tsx
@@ -1,9 +1,30 @@
+import { NavLink } from "react-router-dom"
 
 export default function Header() {
 
     return (
-        <header className=" header ">
-           Header
+        <header className=" header text-accent bg-blue bg-opacity-05 flex flex-space-between">
+            <nav className="flex gap-0" >
+                <NavLink 
+                    to='/' 
+                    className={({isActive}: {isActive: boolean}) => isActive ? "active" : ""}
+                >
+                    Home
+                </NavLink>
+                <NavLink 
+                    to='moviesdb' 
+                    className={({isActive}: {isActive: boolean}) => isActive ? "active" : ""}
+                >
+                    TMDB
+                </NavLink> 
+                <NavLink 
+                    to='movies' 
+                    className={({isActive}: {isActive: boolean}) => isActive ? "active" : ""}
+                >
+                   My Movies
+                </NavLink>
+            </nav>
+            <h3>Login</h3>
         </header>
     );
 }
