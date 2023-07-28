7b690f98ea62cb0ffa3901ac66545aec024e47fb
Raimondas <raimondas.sileikis@gmai.com>
2023-07-28 16:26:39 +0300

    Dashboard

--- movies-db/src/components/Dashboard.tsx
+++ movies-db/src/components/Dashboard.tsx
@@ -1,6 +1,10 @@
-export async function loader() {
-    return null
-  }
+import { redirect} from "react-router-dom"
+import { requireAuth } from "../utils"
+
+export async function loader({ request }: { request: Request }) {
+    await requireAuth(request) 
+    return redirect(new URL(request.url).pathname + "/list")
+}
 
 export default function Dashboard() {
     return null;
